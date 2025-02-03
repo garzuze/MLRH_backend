584aff2289e625a5bb821af0ce2896878eb07c42
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-03 11:18:21 -0300

    Register models in admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Client, ClientContact, Benefit
+
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(ClientContact)
+admin.site.register(Benefit)
