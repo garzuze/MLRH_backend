63d288525eefaf15ad978cc6aa1d6d036bf7b6f8
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-15 08:51:19 -0300

    feat: create personalized admin view on model Client

--- clients/admin.py
+++ clients/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 
 from .models import Client, ClientContact, Benefit, EconomicActivity, Service, ClientFee
 
-# Register your models here.
+class EconomicActivityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
 
-admin.site.register(Client)
+class BenefitAdmin(admin.ModelAdmin):
+    list_display = ['id', 'benefit']
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'corporate_name', 'cnpj', 'cep', 'number_of_employees', 'economic_activity', 'created_at']
+    search_fields = ["cnpj", "corporate_name"]
+    list_filter = ["state"]
+
+admin.site.register(Client, ClientAdmin)
 admin.site.register(ClientContact)
-admin.site.register(Benefit)
-admin.site.register(EconomicActivity)
+admin.site.register(Benefit, BenefitAdmin)
+admin.site.register(EconomicActivity, EconomicActivityAdmin)
 admin.site.register(Service)
 admin.site.register(ClientFee)
