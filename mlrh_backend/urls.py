fa906a4896034864c419e586cf67c492a5514489
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-16 18:21:17 -0300

    refactor: setup jwt authentication

--- mlrh_backend/urls.py
+++ mlrh_backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from clients.views import update_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', update_data),
+    path('', include('main.urls')),
+    path('update_data', update_data)
 ]
