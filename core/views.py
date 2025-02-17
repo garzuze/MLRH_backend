a123412fdeb9922b94dbf667ee98a9e49ce0cede
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-17 09:57:21 -0300

    refactor: setup jwt with Django's User model

--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        email = attrs.get("username")
+        password = attrs.get("password")
+
+        if not email and password:
+            raise self.fail("no_active_acount")
+    
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise self.fail("no_active_account")
+
+        if not user.check_password(password):
+            raise self.fail("no_active_account")
+
+        if not user.is_active:
+            raise self.fail("no_active_account")
+
+        data = super().validate(attrs)
+        data["user"] = {
+            "id": user.id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "is_staff": user.is_staff,
+            "is_superuser": user.is_superuser,
+        }
+        return data
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
