513a053596e89ddc074cec3fc4b1fd9e2dc67e56
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-18 13:46:54 -0300

    refactor: create BenefitViewSet

--- clients/urls.py
+++ clients/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import ClientViewSet
+from .views import *
 
 router = routers.DefaultRouter()
-router.register(r'', ClientViewSet)
+router.register(r'clients', ClientViewSet, basename='client')
+router.register(r'benefits', BenefitViewSet, basename='benefit')
 
 urlpatterns = [
     path('', include(router.urls)),
--- clients/views.py
+++ clients/views.py
@@ -1,8 +1,15 @@
-from .models import Client
-from .serializers import ClientSerializer
+from .models import Client, Benefit, EconomicActivity
+from .serializers import ClientSerializer, BenefitSerializer, EconomicActivitySerializer
 from rest_framework import permissions, viewsets
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class BenefitViewSet(viewsets.ModelViewSet):
+    queryset = Benefit.objects.all()
+    serializer_class = BenefitSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
