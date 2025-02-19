5a02fc2fd3ae947b142abf8c3452c5055b825757
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-19 13:40:04 -0300

    refactor: add endpoint to verify tokens

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from .views import CustomTokenObtainPairView, update_data
 
 urlpatterns = [
     path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("update_data/", update_data, name="update_data")
+    path("update_data/", update_data, name="update_data"),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
