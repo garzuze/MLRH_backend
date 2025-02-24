0577c346649cc42a910ea5a99907490b9d37cded
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-24 09:48:20 -0300

    feat: create personalized admin view on Resume

--- hr/admin.py
+++ hr/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Position, Resume, WorkExperience, Profile, Report
 
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'cpf', 'state', 'email', 'phone']
+    search_fields = ['name", "phone']
+
+
 admin.site.register(Position)
-admin.site.register(Resume)
+admin.site.register(Resume, ResumeAdmin)
 admin.site.register(WorkExperience)
 admin.site.register(Profile)
 admin.site.register(Report)
