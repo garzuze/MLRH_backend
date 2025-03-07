f3e777c83fd420a6871e1e7ffe87396509e36177
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-03-07 08:17:04 -0300

    refactor: remove unused signals.py

--- core/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-
-@receiver(pre_save, sender=User)
-def set_username_as_email(sender, instance, **kwargs):
-    """Ensure username is always set to email before saving"""
-    if not instance.username:
-        instance.username = instance.email
