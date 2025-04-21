775fe36ff1ec234701d89108efb65fe0a8aadcc2
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-04-21 20:52:34 -0300

    feat: create AccountsReceivableTitleViewSet

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets, status
+
+from finance.models import AccountsReceivableTitle
+from finance.serializers import AccountsReceivableTitleSerializer
 
 # Create your views here.
+
+class AccountsReceivableTitleViewSet(viewsets.ModelViewSet):
+    queryset = AccountsReceivableTitle.objects.all()
+    serializer_class = AccountsReceivableTitleSerializer
+    permission_classes = [permissions.IsAdminUser]
--- mlrh_backend/urls.py
+++ mlrh_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('finance/', include('finance.urls')),
     path('clients/', include('clients.urls')),
     path('hr/', include('hr.urls')),
     path('', include('core.urls')),
